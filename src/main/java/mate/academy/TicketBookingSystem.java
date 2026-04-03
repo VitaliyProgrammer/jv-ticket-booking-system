0f6e5c57ae8f0c2867aaf2b272cc0e44072a7952
Vitaliy <vitaliyufc@gmail.com>
2026-04-04 00:18:18 +0300

    Done ticket-booking-system task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,23 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private final Semaphore seats;
 
+    public TicketBookingSystem(int totalSeats) {
+        this.seats = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+
+        boolean seatIsAcquired = seats.tryAcquire();
+
+        if (!seatIsAcquired) {
+            return new BookingResult(user, false, "No seats available!");
+        }
+
+        return new BookingResult(user, true, "Booking is successful!");
     }
 }
